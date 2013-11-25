34d1c2bd115f868820fb3a9b8515ad06e0920efd
Michael Arndt <m_arndt@cs.uni-kl.de>
2013-11-25 14:03:49 +0100

    Add #ifdefs to be able to run the test_voices program if not all backends are available.

--- test/test_voices.cpp
+++ test/test_voices.cpp
@@ -65,6 +65,7 @@
 
 void Run()
 {
+#ifdef _LIB_RRLIB_SPEECH_SYNTHESIS_ESPEAK_PRESENT_
   rrlib::speech_synthesis::tVoice *espeak_default = rrlib::speech_synthesis::tVoiceFactory::Instance().Create("espeak", "default");
   for (unsigned int i = 1; i < 10; ++i)
   {
@@ -72,24 +73,33 @@ void Run()
     espeak_default->Say(text);
     sleep(1);
   }
+#endif
 }
 
 
 int main(int argc, char* argv[])
 {
 
+#ifdef _LIB_RRLIB_SPEECH_SYNTHESIS_FESTIVAL_PRESENT_
   rrlib::speech_synthesis::tVoice *festival_kal = rrlib::speech_synthesis::tVoiceFactory::Instance().Create("festival", "kal_diphone");
   rrlib::speech_synthesis::tVoice *festival_don = rrlib::speech_synthesis::tVoiceFactory::Instance().Create("festival", "don_diphone");
+#endif
+#ifdef _LIB_RRLIB_SPEECH_SYNTHESIS_ESPEAK_PRESENT_
   rrlib::speech_synthesis::tVoice *espeak_mb_en1 = rrlib::speech_synthesis::tVoiceFactory::Instance().Create("espeak", "mb-en1");
+#endif
 
   std::thread thread2(Run);
 
   for (unsigned int i = 1; i < 10; ++i)
   {
     std::string text = "Speaking from thread 1: line number " + std::to_string(i);
+#ifdef _LIB_RRLIB_SPEECH_SYNTHESIS_FESTIVAL_PRESENT_
     festival_kal->Say(text);
     festival_don->Say(text);
+#endif
+#ifdef _LIB_RRLIB_SPEECH_SYNTHESIS_ESPEAK_PRESENT_
     espeak_mb_en1->Say(text);
+#endif
     sleep(1);
   }
 
